2ad20239f5edb0e8e580149e28b4cad0a9115365
pkeen <pkeen7@gmail.com>
2023-11-09 16:08:26 -0700

    remove unneeded api function

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-	runtime: "nodejs", // this is a pre-requisite
-};
-
-export default function handler(request, response) {
-	return response.status(200).json({ text: "I am a Serverless Function!" });
-}
